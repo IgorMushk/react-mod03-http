1df354c174b92d32010fcdaf2fefee938bf64e0b
IgorMushk <igor.mushketik@gmail.com>
2023-09-18 15:01:36 +0300

    add - async componentDidMount

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,33 @@
-export const App = () => {
-  return (
-    <div>
-      React homework template!!
-    </div>
-  );
-};
+import axios from "axios";
+import React, { Component } from "react";
+
+axios.defaults.baseURL = "https://hn.algolia.com/api/v1";
+
+export class App extends Component {
+  state = {
+    articles: [],
+  };
+
+  async componentDidMount() {
+    const response = await axios.get("/search?query=react");
+    this.setState({ articles: response.data.hits });
+    console.log('articles', this.state.articles)
+  }
+
+  render() {
+    const { articles } = this.state;
+    return (
+      <div>
+        {/* articles.length > 0 ? <ArticleList articles={articles} /> : null */}
+      </div>
+    );
+  }
+}
+
+// export const App = () => {
+//   return (
+//     <div>
+//       React homework template!!
+//     </div>
+//   );
+// };
