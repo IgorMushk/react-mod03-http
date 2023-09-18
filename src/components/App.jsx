b4e46296811481fc61d4a1954f5f9af737afcb36
IgorMushk <igor.mushketik@gmail.com>
2023-09-18 15:07:45 +0300

    fix
    <div>
            articles.length > 0 ? <ArticleList articles={articles} /> : null
    </div>
    =================> + {}
          <div>
            {articles.length > 0 ? <ArticleList articles={articles} /> : null}
          </div>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,23 @@ import React, { Component } from "react";
 
 axios.defaults.baseURL = "https://hn.algolia.com/api/v1";
 
+const ArticleList = ({ articles }) => (
+  <ul>
+    {articles.map(({ objectID, url, title }) => (
+      <li key={objectID}>
+        <a href={url} target="_blank" rel="noreferrer noopener">
+          {title}
+        </a>
+      </li>
+    ))}
+  </ul>
+);
+
 export class App extends Component {
   state = {
     articles: [],
   };
+
 
   async componentDidMount() {
     const response = await axios.get("/search?query=react");
@@ -18,7 +31,7 @@ export class App extends Component {
     const { articles } = this.state;
     return (
       <div>
-        {/* articles.length > 0 ? <ArticleList articles={articles} /> : null */}
+        {articles.length > 0 ? <ArticleList articles={articles} /> : null}
       </div>
     );
   }
