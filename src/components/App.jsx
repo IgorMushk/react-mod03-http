16237c69392d4d85edd7eb4b419eb81cde9373da
IgorMushk <igor.mushketik@gmail.com>
2023-09-19 19:10:22 +0300

    del comments

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -66,11 +66,3 @@ export class App extends Component {
     );
   }
 }
-
-// export const App = () => {
-//   return (
-//     <div>
-//       React homework template!!
-//     </div>
-//   );
-// };
